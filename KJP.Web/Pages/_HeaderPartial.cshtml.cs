163ffe58efd243a58fe0661ea785b664af33abef
Kyle Paterson <kyle_paterson@otpp.com>
2019-02-21 16:42:55 -0500

    initial commit #2

---
+++ KJP.Web/Pages/_HeaderPartial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KJP.Web.Pages
+{
+    public class _HeaderPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
